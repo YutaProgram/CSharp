de7973315dba582e92b09e52ef9712e32eb28e6f
YutaPro <102200840+YutaProgram@users.noreply.github.com>
2023-01-20 14:39:10 +0900

    Add files via upload

---
+++ SocketSampleApp/Client/Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using System.Web;
+using System.Net;
+using System.IO;
+
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Class1.Test();
+        }
+    }
+}
