22d423837520b110aee6dbe1932ed3f364e5326b
YutaPro <102200840+YutaProgram@users.noreply.github.com>
2023-01-20 13:57:07 +0900

    Add files via upload

---
+++ HttpConsoleApp/HttpConsoleApp/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace HttpConsoleApp
+{
+    internal class Class1 : MyHttpClient
+    {
+        public Class1(){ }
+
+       public async void GetTest()
+       {
+            //GETメソッドを利用する場合
+            var result = await client.GetAsync(@"https://umayadia-apisample.azurewebsites.net/api/persons/");
+            
+            var json = await result.Content.ReadAsStringAsync();
+            Console.WriteLine($"{(int)result.StatusCode} {result.StatusCode}");
+            Console.WriteLine(json);
+       }
+
+
+        public async void PostTest()
+        {
+            //POSTメソッドを利用する場合
+
+            //ボディの設定
+            var param = new Dictionary<string, object>()
+            {
+                ["name"] = "Joan of Arc",
+                ["note"] = "France",
+                ["age"] = 19,
+                ["registerDate"] = "1412/1/6",
+            };
+
+            //nugetからSystem.Text.jsonをインストールする必要あり
+            //作ったオブジェクトをjson形式にシリアライズ
+            var jsonString = System.Text.Json.JsonSerializer.Serialize(param);
+            var content = new StringContent(jsonString, Encoding.UTF8, @"application/json");
+
+            //POST
+            var result = await client.PostAsync(@"https://umayadia-apisample.azurewebsites.net/api/persons", content);
+            Console.WriteLine($"{(int)result.StatusCode}\n{result.StatusCode}");
+
+            //GET
+            var resultGet = await client.GetAsync(@"https://umayadia-apisample.azurewebsites.net/api/persons");
+            var json = await resultGet.Content.ReadAsStringAsync();
+            Console.WriteLine($"{(int)resultGet.StatusCode}\n{resultGet.StatusCode}");
+            Console.WriteLine(json);
+        }
+    }
+}
---
+++ HttpConsoleApp/HttpConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+using System.Reflection;
+
+namespace HttpConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 sample = new Class1();
+            //sample.GetTest();
+            sample.PostTest();
+
+            Console.ReadLine();
+        }
+    }
+}
